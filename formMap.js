50c5e69c3f0028761815060b400d808218092d71
tsuge1029-droid <tsuge1029@gmail.com>
2026-03-09 16:32:15 +0900

    Add files via upload

---
+++ formMap.js
@@ -0,0 +1,87 @@
+export const formMap = {
+
+  // ザシアン・ザマゼンタ
+  "zacian": "れきせんのゆうしゃ",
+  "zacian-crowned": "けんのおう",
+  "zamazenta": "れきせんのゆうしゃ",
+  "zamazenta-crowned": "たてのおう",
+
+  // フーパ
+  "hoopa": "いましめられし",
+  "hoopa-unbound": "ときはなたれし",
+
+  // メガ
+  "mega": "メガ",
+  "mega-x": "メガX",
+  "mega-y": "メガY",
+  "mega-z": "メガZ",
+
+  // 霊獣系
+  "incarnate": "けしんフォルム",
+  "therian": "れいじゅうフォルム",
+
+  // ミノマダム
+  "plant": "くさきのミノ",
+  "sandy": "すなちのミノ",
+  "trash": "ゴミのミノ",
+
+  // リージョン
+  "alola": "アローラのすがた",
+  "galar": "ガラルのすがた",
+  "hisui": "ヒスイのすがた",
+  "paldea": "パルデアのすがた",
+
+  // シェイミ
+  "shaymin-land": "ランドフォルム",
+  "shaymin-sky": "スカイフォルム",
+
+  // バドレックス
+  "calyrex-chilling": "こくばじょうのすがた",
+  "calyrex-glastrier": "はくばじょうのすがた",
+
+  // ネクロズマ
+  "necrozma-dusk-mane": "たそがれのすがた",
+  "necrozma-dawn-wings": "あかつきのすがた",
+  "necrozma-ultra": "ウルトラフォルム",
+
+  // キュレム
+  "kyurem-black": "ブラックフォルム",
+  "kyurem-white": "ホワイトフォルム",
+  "kyurem": "キュレム",
+
+  // ゲンシカイキ系
+  "primal": "ゲンシカイキ",
+
+  // パルキア系
+  "origin": "オリジンフォルム",
+
+  // ウーラオス
+  "single-strike": "いちげきのすがた",
+  "rapid-strike": "れんげきのすがた",
+
+  // メテノ
+  "core": "コアフォルム",
+  "meteor": "メテオフォルム",
+
+  // チェリム
+  "overcast": "ネガ",
+  "sunshine": "ポジ",
+
+  // ケルディオ
+  "ordinary": "いつものすがた",
+  "resolute": "かくごのすがた",
+
+  // メロエッタ
+  "aria": "ボイス",
+  "pirouette": "ステップ",
+
+  // ロトム
+  "heat": "ヒートロトム",
+  "wash": "ウォッシュロトム",
+  "frost": "フロストロトム",
+  "fan": "スピンロトム",
+  "mow": "カットロトム",
+
+  // ムゲンダイ
+  "eternamax": "ムゲンダイマックス"
+};
